0f3ae425a615c556d4d57610433643ec4e7ca325
Oscar Rompich <oscarligo6@gmail.com>
2025-02-26 22:02:29 -0600

    Pruebas unitarias para la clase DLList.

---
+++ UnitTestsDLL.java
@@ -0,0 +1,57 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+
+public class UnitTestsDLL {
+
+    private DLList<Integer> list;
+
+    @Before
+    public void setUp() {
+        list = new DLList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+    }
+
+    @Test
+
+    public void shouldBeAbletoAddElement() {
+        list.add(6);
+        assertEquals(6, (int) list.deleteRight());
+    }
+
+    @Test
+    public void shouldBeAbletoDeleteLeft() {
+        assertEquals(1, (int) list.deleteLeft());
+    }
+
+    @Test
+    public void shouldBeAbletoDeleteRight() {
+        assertEquals(5, (int) list.deleteRight());
+    }
+
+    @Test
+    public void shouldBeAbletoRemoveAnEspecificItem() {
+        assertEquals(4, (int) list.remove(3));
+    }
+
+    @Test
+    public void shouldBeAbletoPeekAnEspecificItem() {
+        assertEquals(3, (int) list.peek(2));
+    }
+
+
+
+
+
+
+
+
+    
+
+    
+}
